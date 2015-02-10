a07fe068bef07d6045276d957dfad963dd1c1eda
Franca Rast <franca.rast@screenconcept.ch>
2015-02-10 10:41:58 +0100

    make first angular sturr

--- config/application.rb
+++ config/application.rb
@@ -18,9 +18,7 @@ module Receta
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    config.assets.paths << Rails.root.join("vendor","assets","bower_components")
-    config.assets.paths << Rails.root.join("vendor","assets","bower_components","bootstrap-sass-official","assets","fonts")
-
+    config.assets.paths << Rails.root.join("vendor","assets","bower_components","bootstrap-sass-official","assets","fonts","bootstrap-sprockets")
     config.assets.precompile << %r(.*.(?:eot|svg|ttf|woff)$)   # config.i18n.default_locale = :de
   end
 end
